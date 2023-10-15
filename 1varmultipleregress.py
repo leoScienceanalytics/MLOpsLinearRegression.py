4d61bd6391a5cb8742c5c2fc487860e6e923faa1
Leonam Alves <leon14alv@gmail.com>
2023-10-14 23:58:17 -0300

    Modificacao de documento

--- 1varmultipleregress.py
+++ 1varmultipleregress.py
@@ -16,7 +16,7 @@ print(df.describe())
 df.drop(['Unnamed: 0'], axis=1)
 
 
-plt.figure(figsize = (16,8))
+plt.figure(figsize = (10,5))
 plt.scatter(df['TV'], df['sales'], c='red')
 plt.xlabel(" ($) Gasto em propaganda de TV")
 plt.ylabel(" ($) Vendas")
@@ -31,7 +31,10 @@ reg = LinearRegression()
 reg.fit(X, y)
 
 
-print("O modelo é: Vendas = {:.5} + {:.5}X".format(reg.intercept_[0], reg.coef_[0][0]))
+print("O modelo de Regressão --> Sales = {:.5} + {:.5}X".format(reg.intercept_[0], reg.coef_[0][0]))
+
+f_predict = reg.predict(X) #Utilizou o método Predict paa que as previsões fossem feitas
+print('Predict:',f_predict) 
 
 
 
@@ -58,13 +61,61 @@ resultados_calculados = calcular_valores(tv)
 print('Predict :' ,resultados_calculados)
 
 
-plt.figure(figsize = (16,8))
+plt.figure(figsize = (10,5))
 plt.plot(resultados_calculados, c='red')
 plt.xlabel(" ($) Gasto em propaganda de TV")
 plt.ylabel(" ($) Vendas")
 plt.show()
 
 
+plt.figure(figsize = (10,5))
+plt.scatter(
+    df['TV'], 
+    df['sales'], 
+    c='red')
 
 
+plt.plot(
+    df['TV'],
+    f_predict,
+    c='blue',
+    linewidth=3,
+    linestyle=':'
+)
 
+plt.xlabel(" ($) Gasto em propaganda de TV")
+plt.ylabel(" ($) Vendas")
+plt.show()
+
+#Precisão do modelo
+X = df['TV']
+y = df['sales']
+X2 = sm.add_constant(X)#Adiciona constante ao modeo
+est = sm.OLS(y, X2) #Modelo estátístico, método usado para modelar relação entre variáveis
+est2 = est.fit() #Treinando o modelo estátistico
+print(est2.summary()) #Sumário com estatísticas descritivas
+
+#R² = 0.612, signfica que 61.20% da variação das Vendas é explicada pela variação da TV
+#P-Value próximo a 0, pode-se rejeitar a hipótese nula
+
+#Regressão Múltipla
+Xs = df.drop(['sales', 'Unnamed: 0'], axis=1)
+y = df['sales'].values.reshape(-1,1)
+
+
+reg = LinearRegression()
+reg.fit(Xs, y)
+f_multpredict = reg.predict(Xs)
+print('Previsão da Regressão Múltipla:', f_multpredict)
+
+print("O modleo é: Vendas = {:.5} + {:.5}*TV + {:.5}*radio + {:.5}*newspaper".format(reg.intercept_[0], reg.coef_[0][0], reg.coef_[0][1], reg.coef_[0][2]))
+
+
+X = np.column_stack((df['TV'], df['radio'], df['newspaper']))
+y = df['sales']
+
+#Precisão do modelo
+X2 = sm.add_constant(X) #Adiciona costante ao modelo
+est = sm.OLS(y, X2) #Criando um modelo
+est2 = est.fit() #Treinando o modelo estatístico
+print(est2.summary()) #Sumário com estatísticas descritivas
