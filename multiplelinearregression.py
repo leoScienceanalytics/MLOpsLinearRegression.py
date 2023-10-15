a6e1a361f85b3a7f936e0f123b96032d00f5b028
Leonam Alves <leon14alv@gmail.com>
2023-10-15 13:50:13 -0300

    third commit

--- multiplelinearregression.py
+++ multiplelinearregression.py
@@ -22,47 +22,21 @@ plt.xlabel(" ($) Gasto em propaganda de TV")
 plt.ylabel(" ($) Vendas")
 plt.show()
 
-
+#É necessário usar p .values.reshape para que possa ser definida a equação do modelo de regressão linear.
 X = df['TV'].values.reshape(-1,1)
 y = df['sales'].values.reshape(-1,1)
 
 
 reg = LinearRegression()
 reg.fit(X, y)
-
-
-print("O modelo de Regressão --> Sales = {:.5} + {:.5}X".format(reg.intercept_[0], reg.coef_[0][0]))
-
 f_predict = reg.predict(X) #Utilizou o método Predict paa que as previsões fossem feitas
+
+print("O modelo de Regressão --> Sales = {:.5} + {:.5}X".format(reg.intercept_[0], reg.coef_[0][0])) #Função do primeiro grau
 print('Predict:',f_predict) 
 
 
-
-def calcular_valores(z):
-    resultados = []  # Uma lista para armazenar os resultados calculados para cada linha
-    forecast_out = 30
-    
-
-    for w in z:
-        # Realiza o cálculo desejado para cada valor na coluna
-        y = 7.0326 + 0.047537*w 
-        resultados.append(y)  # Adiciona o resultado à lista de resultados
-        valores_desejados = resultados[:forecast_out]  
-
-    return valores_desejados  # Retorna a lista de resultados calculados
-    
-
-#Executando o 'def'
-tv = df['TV']
-resultados_calculados = calcular_valores(tv)
-
-
-# Os resultados calculados para cada linha da coluna serão armazenados em "resultados_calculados"
-print('Predict :' ,resultados_calculados)
-
-
 plt.figure(figsize = (10,5))
-plt.plot(resultados_calculados, c='red')
+plt.plot(f_predict, c='red')
 plt.xlabel(" ($) Gasto em propaganda de TV")
 plt.ylabel(" ($) Vendas")
 plt.show()
@@ -85,7 +59,7 @@ plt.plot(
 
 plt.xlabel(" ($) Gasto em propaganda de TV")
 plt.ylabel(" ($) Vendas")
-plt.show()
+plt.show() #Função do primeiro grau
 
 #Precisão do modelo
 X = df['TV']
@@ -99,18 +73,26 @@ print(est2.summary()) #Sumário com estatísticas descritivas
 #P-Value próximo a 0, pode-se rejeitar a hipótese nula
 
 #Regressão Múltipla
-Xs = df.drop(['sales', 'Unnamed: 0'], axis=1)
-y = df['sales'].values.reshape(-1,1)
+Xs = df.drop(['sales', 'Unnamed: 0'], axis=1) #Determinando colunas que serão treinadas
+y = df['sales'].values.reshape(-1,1) # Determinando colunas que será prevista
 
 
-reg = LinearRegression()
-reg.fit(Xs, y)
-f_multpredict = reg.predict(Xs)
-print('Previsão da Regressão Múltipla:', f_multpredict)
+reg = LinearRegression() #Modelo de Regressão Linear
+reg.fit(Xs, y) #Treinamento do modelo
+f_multpredict = reg.predict(Xs) #Prediccao do modelo
 
 print("O modleo é: Vendas = {:.5} + {:.5}*TV + {:.5}*radio + {:.5}*newspaper".format(reg.intercept_[0], reg.coef_[0][0], reg.coef_[0][1], reg.coef_[0][2]))
+print('Previsão da Regressão Múltipla:', f_multpredict)
+
+plt.figure(figsize = (10,5))
+plt.plot(f_multpredict, c='red')
+plt.ylabel(" ($) Vendas")
+plt.show()
 
 
+
+
+#Modelo de Regressão pelo Métodos OLS ----- Usado para medir precisão do Modelo de Regressão Linear Múltipla.
 X = np.column_stack((df['TV'], df['radio'], df['newspaper']))
 y = df['sales']
 
@@ -119,3 +101,5 @@ X2 = sm.add_constant(X) #Adiciona costante ao modelo
 est = sm.OLS(y, X2) #Criando um modelo
 est2 = est.fit() #Treinando o modelo estatístico
 print(est2.summary()) #Sumário com estatísticas descritivas
+
+
