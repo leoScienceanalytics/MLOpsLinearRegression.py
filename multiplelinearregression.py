314c584fd6fee79f336813e74dc26ff51e326b68
Leonam Alves <leon14alv@gmail.com>
2023-10-17 01:24:20 -0300

    document updated

--- multiplelinearregression.py
+++ multiplelinearregression.py
@@ -18,6 +18,10 @@ df = df.drop(['Unnamed: 0'], axis=1)
 print(df)
 print(df.describe())
 
+
+
+#Modelo estatístico e métricas de precisão (3 Var independ.)
+
 #Teste de Multicolinearidade e Dimensionalidade 
 #Correlação das variáveis independentes
 print(df.corr()) #Multicolinearidade
@@ -33,67 +37,49 @@ mean_mse = mse_scores.mean()
 print('Média dos erros: ',mean_mse) # mean_mse = 3.07 ----- Deve-se normalizar os dados, realizando feature engineering.
 #Variáveis independentes possuem dados de diferentes escalas, isso pode fazer com que o modelo não seja preciso, deve reduzir a dimensionalidade.
 
-#Feature engineering
 
-
-
-
-
-
-
-
-
-
-
-
-
-plt.figure(figsize = (10,5))
-plt.scatter(df['TV'], df['sales'], c='red')
-plt.xlabel(" ($) Gasto em propaganda de TV")
-plt.ylabel(" ($) Vendas")
-plt.show()
-
-#Construção do modelo de Regressão Linear
-#É necessário usar p .values.reshape para que possa ser definida a equação do modelo de regressão linear.
-X = df['TV'].values.reshape(-1,1)
-y = df['sales'].values.reshape(-1,1)
-
-#Regressão Linear Simples
-reg = LinearRegression()
-reg.fit(X, y)
-f_predict = reg.predict(X) #Utilizou o método Predict paa que as previsões fossem feitas
-
-print("O modelo de Regressão --> Sales = {:.5} + {:.5}X".format(reg.intercept_[0], reg.coef_[0][0])) #Função do primeiro grau
-print('Predict:',f_predict) 
-
-
-plt.figure(figsize = (10,5))
-plt.plot(f_predict, c='red')
-plt.xlabel(" ($) Gasto em propaganda de TV")
-plt.ylabel(" ($) Vendas")
-plt.show()
-
-
-plt.figure(figsize = (10,5))
-plt.scatter(df['TV'], df['sales'], c='red')
-
-
-plt.plot( df['TV'], f_predict, c='blue', linewidth=3, linestyle=':')
-
-plt.xlabel(" ($) Gasto em propaganda de TV")
-plt.ylabel(" ($) Vendas")
-plt.show() #Função do primeiro grau
-
-#Precisão do modelo
-X = df['TV']
+#Construção do modelo estatísico 
+#Modelo de Regressão pelo Métodos OLS ----- Usado para medir precisão do Modelo de Regressão Linear Múltipla.
+X = np.column_stack((df['TV'], df['radio'], df['newspaper']))
 y = df['sales']
-X2 = sm.add_constant(X)#Adiciona constante ao modeo
-est = sm.OLS(y, X2) #Modelo estátístico, método usado para modelar relação entre variáveis
-est2 = est.fit() #Treinando o modelo estátistico
+X2 = sm.add_constant(X) #Adiciona costante ao modelo
+est = sm.OLS(y, X2) #Criando um modelo
+est2 = est.fit() #Treinando o modelo estatístico
 print(est2.summary()) #Sumário com estatísticas descritivas
 
-#R² = 0.612, signfica que 61.20% da variação das Vendas é explicada pela variação da TV
-#P-Value próximo a 0, pode-se rejeitar a hipótese nula
+
+#Modelo estatístico e métricas de precisão (3 Var independ.)
+#Feature engineering
+#Variáveis independentes possuem dados de diferentes escalas, isso pode fazer com que o modelo não seja preciso.
+
+
+#Validção Cruzada ----- Tests como forma de analisar a precisão do modelo, quanto maior for o erro, maior a dimensionalidade(Métrica de precisão).
+x_train = df.drop(['sales','newspaper'], axis=1)
+y_train = df['sales']
+model = LinearRegression() #Dimensionalidade
+scores = cross_val_score(model, x_train, y_train, cv=5, scoring='neg_mean_squared_error')
+mse_scores = -scores
+mean_mse = mse_scores.mean()
+print('Média dos erros: ',mean_mse)
+
+#Construção do modelo estatístico
+#Modelo de Regressão pelo Métodos OLS ----- Usado para medir precisão do Modelo de Regressão Linear Múltipla.
+X = np.column_stack((df['TV'], df['radio']))
+y = df['sales']
+#Precisão do modelo
+X2 = sm.add_constant(X) #Adiciona costante ao modelo
+est = sm.OLS(y, X2) #Criando um modelo
+est2 = est.fit() #Treinando o modelo estatístico
+print(est2.summary()) #Sumário com estatísticas descritivas
+
+
+
+
+
+
+
+
+
 
 
 
@@ -114,14 +100,6 @@ plt.ylabel(" ($) Vendas")
 plt.show()
 
 #Modelo de Regressão pelo Métodos OLS ----- Usado para medir precisão do Modelo de Regressão Linear Múltipla.
-X = np.column_stack((df['TV'], df['radio'], df['newspaper']))
-y = df['sales']
-
-#Precisão do modelo
-X2 = sm.add_constant(X) #Adiciona costante ao modelo
-est = sm.OLS(y, X2) #Criando um modelo
-est2 = est.fit() #Treinando o modelo estatístico
-print(est2.summary()) #Sumário com estatísticas descritivas
 
 
 
@@ -142,12 +120,3 @@ plt.plot(f_multpredict, c='red')
 plt.ylabel(" ($) Vendas")
 plt.show()
 
-#Modelo de Regressão pelo Métodos OLS ----- Usado para medir precisão do Modelo de Regressão Linear Múltipla.
-X = np.column_stack((df['TV'], df['radio']))
-y = df['sales']
-
-#Precisão do modelo
-X2 = sm.add_constant(X) #Adiciona costante ao modelo
-est = sm.OLS(y, X2) #Criando um modelo
-est2 = est.fit() #Treinando o modelo estatístico
-print(est2.summary()) #Sumário com estatísticas descritivas
